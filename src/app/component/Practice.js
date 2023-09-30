73302640b44bf7878e361aa6a1cecb8b59cc7531
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-30 17:44:05 +0500

    'tailwind'

--- src/app/component/Practice.js
+++ src/app/component/Practice.js
@@ -5,7 +5,7 @@ const Practice = () => {
   return (
     <>
       <div className="container mx-auto">
-        <div className="row pt-32 grid lg:grid-cols-2 gap-10">
+        <div className="row pt-32 grid lg:grid-cols-2 md:grid-cols-2 gap-10">
             <div className='bg-indigo-600 text-white rounded-md font-medium mt-5 mb-5 p-12'>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, eveniet impedit. Rem praesentium natus veniam quis, dignissimos iste laboriosam exercitationem eos ex obcaecati asperiores molestias nihil illum. Neque, atque culpa.
             </div>
@@ -30,7 +30,7 @@ const Practice = () => {
       </div>
 
       <div className="container mx-auto">
-        <div className="row grid lg:grid-cols-4 gap-10">
+        <div className="row grid lg:grid-cols-4 gap-8">
             <div className='bg-indigo-600 text-white rounded-md font-medium mt-5 mb-5 p-12'>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, eveniet impedit. Rem praesentium natus veniam quis, dignissimos iste laboriosam exercitationem eos ex obcaecati asperiores molestias nihil illum. Neque, atque culpa.
             </div>

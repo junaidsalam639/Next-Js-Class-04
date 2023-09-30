5ad10b7ebc85de59a46b47aec9652f2d26262418
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-30 19:52:29 +0500

    'tailwind_css'

--- src/app/component/Home_Page/FeatureProduct.js
+++ src/app/component/Home_Page/FeatureProduct.js
@@ -5,41 +5,38 @@ const FeatureProduct = () => {
   return (
     <div>
       <div className="container mx-auto">
+        <h1 className='text-4xl font-bold text-center'>Featured Products</h1>
         <div className="card-container grid lg:grid-cols-4 gap-12">
-        <div className="card w-80 shadow-md border-2 border-gray-200 p-5 rounded-md mt-4 mb-4">
+        <div className="card w-72 shadow-md border-2 border-gray-200 p-5 rounded-md mt-4 mb-4">
                 <img src='https://ak1.ostkcdn.com/images/products/is/images/direct/1d3770cad1c35c3c792599cbaad2a1bbf640539a/Modern-3-Seater-Sofa-With-Wood-Legs-for-Living-Room-and-Bedroom.jpg' className='w-80 h-56 rounded-md shadow-md' />
-                <div className="card-info pt-4">
-                    <h2><b>Category : </b> </h2>
-                    <h2><b>Price : </b> </h2>
-                    <h2><b>Title : </b> </h2>
-                    <h2><b>Description : </b></h2>
+                <div className="card-info pt-4 text-center">
+                    <h2><b>Cantelever Chair</b> </h2>
+                    <h2><b>Code : Y523201</b> </h2>
+                    <h2><b>$42.00</b> </h2>
                 </div>
             </div>
-        <div className="card w-80 shadow-md border-2 border-gray-200 p-5 rounded-md mt-4 mb-4">
+        <div className="card w-72 shadow-md border-2 border-gray-200 p-5 rounded-md mt-  mb-4">
                 <img src='https://ak1.ostkcdn.com/images/products/is/images/direct/1d3770cad1c35c3c792599cbaad2a1bbf640539a/Modern-3-Seater-Sofa-With-Wood-Legs-for-Living-Room-and-Bedroom.jpg' className='w-80 h-56 rounded-md shadow-md' />
-                <div className="card-info pt-4">
-                    <h2><b>Category : </b> </h2>
-                    <h2><b>Price : </b> </h2>
-                    <h2><b>Title : </b> </h2>
-                    <h2><b>Description : </b></h2>
+                <div className="card-info pt-4 text-center">
+                    <h2><b>Cantelever Chair </b> </h2>
+                    <h2><b>Code : Y523201 </b> </h2>
+                    <h2><b>$42.00</b> </h2>
                 </div>
             </div>
-        <div className="card w-80 shadow-md border-2 border-gray-200 p-5 rounded-md mt-4 mb-4">
+        <div className="card w-72 shadow-md border-2 border-gray-200 p-5 rounded-md mt-4 mb-4">
                 <img src='https://ak1.ostkcdn.com/images/products/is/images/direct/1d3770cad1c35c3c792599cbaad2a1bbf640539a/Modern-3-Seater-Sofa-With-Wood-Legs-for-Living-Room-and-Bedroom.jpg' className='w-80 h-56 rounded-md shadow-md' />
-                <div className="card-info pt-4">
-                    <h2><b>Category : </b> </h2>
-                    <h2><b>Price : </b> </h2>
-                    <h2><b>Title : </b> </h2>
-                    <h2><b>Description : </b></h2>
+                <div className="card-info pt-4 text-center">
+                    <h2><b>Cantelever Chair</b> </h2>
+                    <h2><b>Code : Y523201</b> </h2>
+                    <h2><b>$42.00</b> </h2>
                 </div>
             </div>
-        <div className="card w-80 shadow-md border-2 border-gray-200 p-5 rounded-md mt-4 mb-4">
+        <div className="card w-72 shadow-md border-2 border-gray-200 p-5 rounded-md mt-4 mb-4">
                 <img src='https://ak1.ostkcdn.com/images/products/is/images/direct/1d3770cad1c35c3c792599cbaad2a1bbf640539a/Modern-3-Seater-Sofa-With-Wood-Legs-for-Living-Room-and-Bedroom.jpg' className='w-80 h-56 rounded-md shadow-md' />
-                <div className="card-info pt-4">
-                    <h2><b>Category : </b> </h2>
-                    <h2><b>Price : </b> </h2>
-                    <h2><b>Title : </b> </h2>
-                    <h2><b>Description : </b></h2>
+                <div className="card-info pt-4 text-center">
+                    <h2><b>Cantelever Chair </b> </h2>
+                    <h2><b>Code : Y523201 </b> </h2>
+                    <h2><b>$42.00</b> </h2>
                 </div>
             </div>
             

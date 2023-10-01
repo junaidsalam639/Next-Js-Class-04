432f7fa57ed92dedffea37afbb5987b1a5b8e99b
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-01 18:25:53 +0500

    'koshish'

---
+++ src/app/[pages]/page.js
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import productApi from '../Api_Data_Fecth/getProduct'
+const page = async({params}) => {
+  const wait = await productApi();
+  console.log(wait.products[params.pages - 1]);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
--- src/app/component/Home_Page/FeatureProduct.js
+++ src/app/component/Home_Page/FeatureProduct.js
@@ -1,13 +1,15 @@
 'use client'
 import React , { useState } from 'react'
 import productApi from '@/app/Api_Data_Fecth/getProduct'
+import Link from 'next/link';
 
 const FeatureProduct = () => {
     const [cards, setCards] = useState([]);
     const submit = async () => {
         const data = await productApi();
         const cardElements = data.products.map((user, index) => (
-            <div key={index} className="card w-80 h-full shadow-md border-2 border-gray-200 p-5 rounded-md mt-4 mb-4">
+            <Link key={index} href={`/${user.id}`}>
+            <div className="card w-80 h-full shadow-md border-2 border-gray-200 p-5 rounded-md mt-4 mb-4">
                 <img src={user.thumbnail} alt={user.name} className='w-80 h-56 rounded-md shadow-md' />
                 <div className="card-info pt-4">
                     <h2><b>Category : </b> {user.category}</h2>
@@ -16,6 +18,7 @@ const FeatureProduct = () => {
                     <h2><b>Description : </b> {user.description}</h2>
                 </div>
             </div>
+            </Link>
         ));
         setCards(cardElements);
     }

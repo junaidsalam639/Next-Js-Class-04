fef6c86dc778696c083e977d6c9908099d7df1ba
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-01 23:55:43 +0500

    'flase'

--- src/app/All_Page/ShopList/page.js
+++ src/app/All_Page/ShopList/page.js
@@ -1,3 +1,4 @@
+import Footer_Footer from '@/app/component/Footer'
 import Fronted from '@/app/component/Fronted'
 import NavbarOne from '@/app/component/Home_Page/NavbarOne'
 import Nav from '@/app/component/Nav'
@@ -5,15 +6,15 @@ import Shop from '@/app/component/shopp/Shop'
 import React from 'react'
 
 const page = () => {
-  return (
-    <>
-      <NavbarOne />
-      <Nav />
-    <Fronted />
-      <Shop />
-      <Footer_Footer />
-    </>
-  )
+    return (
+        <>
+            <NavbarOne />
+            <Nav />
+            <Fronted />
+            <Shop />
+            <Footer_Footer />
+        </>
+    )
 }
 
 export default page

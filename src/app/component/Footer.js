9d61a7f819f176477fff740b2bbf88ac0bed884e
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-03 16:01:45 +0500

    'contact'

--- src/app/component/Footer.js
+++ src/app/component/Footer.js
@@ -11,7 +11,7 @@ const Footer_Footer = () => {
     <div class="lg:w-1/4 md:w-1/2 w-full px-4">
         <h2 class="title-font font-bold text-gray-900 tracking-widest text-2xl mb-3">Hekto</h2>
         <div class="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
-          <div class="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+          <div class="w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
             <input type="text" id="footer-field" name="footer-field" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder='Enter Email Adress'/>
           </div>
           {/* <button class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-pink-600 border-0 py-2 px-6 focus:outline-none hover:bg-pink-500 rounded">Singup</button> */}
